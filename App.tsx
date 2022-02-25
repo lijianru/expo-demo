92a557e0ae013fb2cd8189a225954966c8d6ab7c
lijianru <2219269374@qq.com>
2022-02-25 11:40:02 +0800

    dashboard

--- App.tsx
+++ App.tsx
@@ -1,194 +1,98 @@
 import React, { useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {
-  NativeBaseProvider,
-  Button,
-  Checkbox,
-  HStack,
-  Select,
-  CheckIcon,
+  Actionsheet,
   Box,
-  Slider,
-  Badge,
-  Progress,
-  Heading,
-  FormControl,
-  Input,
-  Modal,
-  ScrollView,
+  Button,
   Center,
+  NativeBaseProvider,
+  useDisclose,
+  Text,
+  PresenceTransition,
+  HStack,
+  Progress,
 } from 'native-base';
 import { theme } from './customerTheme';
 
 function App() {
-  const [service, setService] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [showModal2, setShowModal2] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclose();
+  const [toggle, setToggle] = useState(false);
 
   return (
     <NativeBaseProvider theme={theme}>
       <SafeAreaProvider style={{ marginTop: 50, marginBottom: 50 }}>
-        <ScrollView>
-          <Center
-            bg="emerald.400"
-            _text={{
-              color: 'white',
-            }}
-            rounded="xl"
-            w={[100, 200, 300, 400, 500]}
-            h={24}
-          >
-            This is a box
-          </Center>
+        <Center>
+          <Button onPress={onOpen}>Actionsheet</Button>
+          <Actionsheet isOpen={true} disableOverlay>
+            <Actionsheet.Content>
+              <Box w="100%" h={60} px={4} justifyContent="center">
+                <Text
+                  fontSize="16"
+                  color="gray.500"
+                  _dark={{
+                    color: 'gray.300',
+                  }}
+                >
+                  Albums
+                </Text>
+              </Box>
+              <Actionsheet.Item>Delete</Actionsheet.Item>
+              <Actionsheet.Item>Share</Actionsheet.Item>
+              <Actionsheet.Item>Play</Actionsheet.Item>
+              <Actionsheet.Item>Favourite</Actionsheet.Item>
+              <Actionsheet.Item>Cancel</Actionsheet.Item>
+            </Actionsheet.Content>
+          </Actionsheet>
+        </Center>
 
-          <Box m="2">
-            <Button onPress={() => {}} _stack={{ margin: 2 }}>
-              Click
-            </Button>
-          </Box>
-
-          <Box m="2">
-            <HStack space={6}>
-              <Checkbox value="test" accessibilityLabel="This is a dummy checkbox" />
-              <Checkbox
-                value="test"
-                accessibilityLabel="This is a dummy checkbox"
-                defaultIsChecked
-              />
-            </HStack>
-          </Box>
-
-          <Box m="2">
-            <Select
-              selectedValue={service}
-              minWidth="200"
-              accessibilityLabel="Choose Service"
-              placeholder="Choose Service"
-              _selectedItem={{
-                bg: 'teal.600',
-                endIcon: <CheckIcon size="5" />,
+        <Center mt={4}>
+          <HStack>
+            <PresenceTransition
+              visible={toggle}
+              initial={{
+                translateX: 0,
               }}
-              mt={1}
-              onValueChange={(itemValue) => setService(itemValue)}
+              animate={{
+                translateX: 200,
+                transition: {
+                  duration: 250,
+                },
+              }}
             >
-              <Select.Item label="UX Research" value="ux" />
-              <Select.Item label="Web Development" value="web" />
-              <Select.Item label="Cross Platform Development" value="cross" />
-              <Select.Item label="UI Designing" value="ui" />
-              <Select.Item label="Backend Development" value="backend" />
-            </Select>
-          </Box>
-
-          <Box m="2">
-            <Slider
-              defaultValue={70}
-              minValue={0}
-              maxValue={100}
-              accessibilityLabel="hello world"
-              step={1}
+              <Text w={100} bg={'amber.100'}>
+                Pick up
+              </Text>
+            </PresenceTransition>
+            <Button w={100} onPress={() => setToggle(!toggle)}>
+              XX
+            </Button>
+            <PresenceTransition
+              visible={toggle}
+              initial={{
+                translateX: 0,
+              }}
+              animate={{
+                translateX: -200,
+                transition: {
+                  duration: 250,
+                },
+              }}
             >
-              <Slider.Track>
-                <Slider.FilledTrack />
-              </Slider.Track>
-              <Slider.Thumb />
-            </Slider>
-          </Box>
+              <Text w={100} bg={'amber.100'}>
+                Drop off
+              </Text>
+            </PresenceTransition>
+          </HStack>
+        </Center>
 
-          <Box m="2">
-            <Badge>NEW FEATURE</Badge>
+        <Center mt={4}>
+          <Box w={300} bg={'blueGray.300'} rounded="md">
+            <Text>1123123</Text>
+            <Text>1123123</Text>
+            <Text>1123123</Text>
+            <Progress rounded="0" roundedBottomLeft={'md'} roundedBottomRight={'md'} />
           </Box>
-
-          <Box m="2">
-            <Progress value={45} max={100} />
-          </Box>
-
-          <Box m="2">
-            <Button onPress={() => setShowModal(true)}>Show Modal 1</Button>
-            <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-              <Modal.Content maxWidth="400px">
-                <Modal.CloseButton />
-                <Modal.Header>Contact Us</Modal.Header>
-                <Modal.Body>
-                  <FormControl>
-                    <FormControl.Label>Name</FormControl.Label>
-                    <Input />
-                  </FormControl>
-                  <FormControl mt="3">
-                    <FormControl.Label>Email</FormControl.Label>
-                    <Input />
-                  </FormControl>
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button.Group space={2}>
-                    <Button
-                      variant="ghost"
-                      colorScheme="blueGray"
-                      onPress={() => {
-                        setShowModal(false);
-                      }}
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      onPress={() => {
-                        setShowModal(false);
-                        setShowModal2(true);
-                      }}
-                    >
-                      Open Modal 2
-                    </Button>
-                  </Button.Group>
-                </Modal.Footer>
-              </Modal.Content>
-            </Modal>
-            <Modal isOpen={showModal2} onClose={() => setShowModal2(false)}>
-              <Modal.Content maxWidth="400px">
-                <Modal.CloseButton />
-                <Modal.Header>Contact Us 2</Modal.Header>
-                <Modal.Body>
-                  <FormControl>
-                    <FormControl.Label>Name 2</FormControl.Label>
-                    <Input />
-                  </FormControl>
-                  <FormControl mt="3">
-                    <FormControl.Label>Email 2</FormControl.Label>
-                    <Input />
-                  </FormControl>
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button.Group space={2}>
-                    <Button
-                      variant="ghost"
-                      colorScheme="blueGray"
-                      onPress={() => {
-                        setShowModal2(false);
-                      }}
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      onPress={() => {
-                        setShowModal2(false);
-                        setShowModal(true);
-                      }}
-                    >
-                      Open Modal 1
-                    </Button>
-                  </Button.Group>
-                </Modal.Footer>
-              </Modal.Content>
-            </Modal>
-          </Box>
-
-          <Heading size="xs">xs</Heading>
-          <Heading size="sm">sm</Heading>
-          <Heading size="md">md</Heading>
-          <Heading size="lg">lg</Heading>
-          <Heading size="xl">xl</Heading>
-          <Heading size="2xl">2xl</Heading>
-          <Heading size="3xl">3xl</Heading>
-          <Heading size="4xl">4xl</Heading>
-        </ScrollView>
+        </Center>
       </SafeAreaProvider>
     </NativeBaseProvider>
   );
