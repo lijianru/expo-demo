b0d83fd01856eefd7c0dca00490ddcf0e8cded42
lijianru <2219269374@qq.com>
2022-02-27 17:50:46 +0800

    tailwindcss

--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { Platform, StyleSheet } from 'react-native';
+import tw from 'twrnc';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function ModalScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View style={tw`flex items-center justify-center`}>
+      <Text style={tw`text-xl font-bold`}>Modal</Text>
+      <View style={tw`m-8 h-px w-4/5`} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/ModalScreen.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
@@ -17,20 +18,3 @@ export default function ModalScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
